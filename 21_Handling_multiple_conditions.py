9d8230515f8830e840b2e184b8e97554fc2f3e6a
Zuzana Dostálová <zuzanadostalova@dhcp-78-086.eduroam.muni.cz>
2019-12-05 16:04:14 +0100

    add tutorial

--- 21_Handling_multiple_conditions.py
+++ 21_Handling_multiple_conditions.py
@@ -9,10 +9,21 @@
 # print("Tax rate is: " + str(tax))
 
 # Use elif to add a default action
+# province = input("Enter your province: ")
+# if province == "Alberta":
+#     tax = 0.05
+# elif province == "Nunavut":
+#     tax = 0.05
+# elif province == "Ontario":
+#     tax = 0.13
+# else:
+#     tax = 0.15
+# print("Tax rate is: " + str(tax))
+
+# Mutltiple conditions by same the action -> combine them into a single one
 province = input("Enter your province: ")
-if province == "Alberta":
-    tax = 0.05
-elif province == "Nunavut":
+if province == "Alberta" \
+   or province == "Nunavut":
     tax = 0.05
 elif province == "Ontario":
     tax = 0.13
