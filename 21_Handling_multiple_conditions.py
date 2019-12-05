e62ae22d7eedb89301f7d6e7cce52c5abef19052
Zuzana Dostálová <zuzanadostalova@dhcp-78-086.eduroam.muni.cz>
2019-12-05 15:52:09 +0100

    add tutorial

--- 21_Handling_multiple_conditions.py
+++ 21_Handling_multiple_conditions.py
@@ -1,4 +1,14 @@
 #If only one of the statements will occur, use elif
+# province = input("Enter your province: ")
+# if province == "Alberta":
+#     tax = 0.05
+# elif province == "Nunavut":
+#     tax = 0.05
+# elif province == "Ontario":
+#     tax = 0.13
+# print("Tax rate is: " + str(tax))
+
+# Use elif to add a default action
 province = input("Enter your province: ")
 if province == "Alberta":
     tax = 0.05
@@ -6,4 +16,6 @@ elif province == "Nunavut":
     tax = 0.05
 elif province == "Ontario":
     tax = 0.13
+else:
+    tax = 0.15
 print("Tax rate is: " + str(tax))
