ec507558b90894f5b0df6389b5b61c41376fee1e
Zuzana Dostálová <zuzanadostalova@dhcp-78-086.eduroam.muni.cz>
2019-12-05 16:44:21 +0100

    add tutorial

--- 21_Handling_multiple_conditions.py
+++ 21_Handling_multiple_conditions.py
@@ -21,12 +21,38 @@
 # print("Tax rate is: " + str(tax))
 
 # Mutltiple conditions by same the action -> combine them into a single one
-province = input("Enter your province: ")
-if province == "Alberta" \
-   or province == "Nunavut":
-    tax = 0.05
-elif province == "Ontario":
-    tax = 0.13
+# province = input("Enter your province: ")
+# if province == "Alberta" \
+#    or province == "Nunavut":
+#     tax = 0.05
+# elif province == "Ontario":
+#     tax = 0.13
+# else:
+#     tax = 0.15
+# print("Tax rate is: " + str(tax))
+
+# In Operator
+# province = input("Enter your province: ")
+# if province in("Alberta", \
+#                "Nunavut", "Yukon"):
+#     tax = 0.05
+# elif province == "Ontario":
+#     tax = 0.13
+# else:
+#     tax = 0.15
+# print("Tax rate is: " + str(tax))
+
+# Action depending on a combination of conditions, you can nest if statements
+country = input("Enter the name of your home country:  ")
+if country == "Canada":
+    province = input("Enter the name of your province: ")
+    if province in("Alberta", \
+               "Nunavut", "Yukon"):
+        tax = 0.05
+    elif province == "Ontario":
+        tax = 0.13
+    else:
+        tax = 0.15
 else:
-    tax = 0.15
+    tax = 0.0
 print("Tax rate is: " + str(tax))
