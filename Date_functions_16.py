44c1ead9213123cf39e5ffe170c7723c56771c05
zuzanadostalova <58548292+zuzanadostalova@users.noreply.github.com>
2019-12-05 14:37:48 +0100

    Update Date_functions_16.py

--- Date_functions_16.py
+++ Date_functions_16.py
@@ -23,3 +23,16 @@ print("Year: " + str(today.year))
 print("Hour: " + str(today.hour))
 print("Minute: " + str(today.minute))
 print("Second: " + str(today.second))
+
+
+# Input date
+from datetime import datetime, timedelta
+birthday = input("When is your birthday (dd/mm/yyyy)? ")
+
+birthday_date = datetime.strptime(birthday, "%d/%m/%Y")
+
+print("Birthday: " + str(birthday_date))
+
+one_day = timedelta (days=1)
+birthday_eve = birthday_date - one_day
+print("Day before birthday: " + str(birthday_eve))
