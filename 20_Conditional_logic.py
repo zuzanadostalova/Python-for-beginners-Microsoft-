9cfd012637af664ee16a76255c1c6d2b16bb2e27
Zuzana Dostálová <zuzanadostalova@dhcp-78-086.eduroam.muni.cz>
2019-12-05 15:19:38 +0100

    add tutorial

--- 20_Conditional_logic.py
+++ 20_Conditional_logic.py
@@ -1,42 +1,42 @@
-# # Check tax
-# price = input("How much did you pay? ")
+# Check tax
+price = input("How much did you pay? ")
 
-# price = float(price)
-# if price >= 1.00:
-#     tax = .07
-#     print("Tax rate is: " + str(tax))
+price = float(price)
+if price >= 1.00:
+    tax = .07
+    print("Tax rate is: " + str(tax))
 
-# # Add else I.
-# price = input("How much did you pay? ")
+# Add else I.
+price = input("How much did you pay? ")
 
-# price = float(price)
-# if price >= 1.00:
-#     tax = .07
-#     print("Tax rate is: " + str(tax))
-# else:
-#     tax = 0
-#     print("Tax rate is: " + str(tax))
+price = float(price)
+if price >= 1.00:
+    tax = .07
+    print("Tax rate is: " + str(tax))
+else:
+    tax = 0
+    print("Tax rate is: " + str(tax))
 
-# Add else II.
-# price = input("How much did you pay? ")
+Add else II.
+price = input("How much did you pay? ")
 
-# price = float(price)
-# if price >= 1.00:
-#     tax = .07
-# else:
-#     tax = 0
-# print("Tax rate is: " + str(tax))
+price = float(price)
+if price >= 1.00:
+    tax = .07
+else:
+    tax = 0
+print("Tax rate is: " + str(tax))
 
-# Comparing strings
-# country = input("Enter the name of your home country:  ")
-# if country == "canada":
-#     print("So you must like hockey!")
-# else:
-#     print("You are not from Canda")
+Comparing strings
+country = input("Enter the name of your home country:  ")
+if country == "canada":
+    print("So you must like hockey!")
+else:
+    print("You are not from Canda")
 
-#will not work if you enter first capital letter "Canada"
+will not work if you enter first capital letter "Canada"
 
-# How to correct runtime error? ->
+How to correct runtime error? ->
 country = input("Enter the name of your home country:  ")
 if country.lower() == "canada":
     print("So you must like hockey!")
