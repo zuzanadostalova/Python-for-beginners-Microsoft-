77e5f995cc9b6293bfb6db90942d4b3d0fcb3382
zuzanadostalova <58548292+zuzanadostalova@users.noreply.github.com>
2019-12-05 14:36:00 +0100

    Update Date_functions_16.py

--- Date_functions_16.py
+++ Date_functions_16.py
@@ -1,4 +1,4 @@
-
+# Date functions
 from datetime import datetime, timedelta
 today = datetime.now()
 print("Today is: " + str(today))
@@ -10,3 +10,16 @@ print("Today is: " + str(today))
 one_week = timedelta(weeks=1)
 last_week = today - one_week
 print("Last week was: " + str(last_week))
+
+
+# Format date
+from datetime import datetime
+today = datetime.now()
+
+print("Day: " + str(today.day))
+print("Month: " + str(today.month))
+print("Year: " + str(today.year))
+
+print("Hour: " + str(today.hour))
+print("Minute: " + str(today.minute))
+print("Second: " + str(today.second))
