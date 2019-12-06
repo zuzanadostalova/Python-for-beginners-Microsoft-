8480464be2aa27d103b0ea59aa5fb53237cbb9fe
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2019-12-06 14:26:58 +0100

    ...

--- 01_Helloworld.py
+++ 01_Helloworld.py
@@ -1,2 +1,21 @@
-name = input("Please enter your name: ")
-print(name)
+# 1. lesson - The introduction of the speakers, the course 
+# useful links - GitHub repository "https://github.com/microsoft/c9-python-getting-started"
+# Introduction to Python https://docs.microsoft.com/en-us/learn/modules/intro-to-python/
+
+# 2. lesson - Theoretical introduction to Python
+# Flexible programming language
+# Designed to be human readable
+# Great starter language
+# Great advanced language
+# Wonderful community
+# Wide range of application
+
+# 3. lesson - Getting started with Python installation
+# Interpreter for Python - https://www.python.org/downloads/; more info https://www.python.org/doc/
+# Editor for Python - https://code.visualstudio.com/, more info https://code.visualstudio.com/docs/languages/python
+# Extension https://marketplace.visualstudio.com/items?itemName=ms-python.python
+
+# 4. lesson  - Configuring the visual studio
+# The download of the Python extension into Visual studio code
+# Introduction to the Visual studio code
+
---
+++ 02_Print.py
@@ -0,0 +1,33 @@
+# 5. lesson - Print
+# create a new folder in your laptop
+# create a new file in VSC, name it name.py, and save it in said folder
+# always save your file with the new code before printing
+# when you need to print, write in the terminal: python3 name.py
+
+# you can '' or "" but be consistent
+print ('Hello world single quotes')
+print ("Hello world double quotes")
+
+name = input("Please enter your name: ")
+print(name)
+
+print ("Hello world")
+
+# () provides a blank line
+print()
+print("Did you see that blank line?")
+
+# \n provides blank in the middle of a string
+print("Blank line \nin the middle of string")
+
+# Debugging with print
+print("Adding numbers")
+x = 42 + 206
+print("Performing division")
+y = x/0
+print("Math complete")
+
+# Notes: 
+# comment in MacOs - cmd + / on marked text
+# if you do not want to print all, comment the text you do not want to print out
+
