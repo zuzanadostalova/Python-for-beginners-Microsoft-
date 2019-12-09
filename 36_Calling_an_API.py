783be215340c10a3922381de6ad8995267920914
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2019-12-09 02:02:43 +0100

    ...

---
+++ 36_Calling_an_API.py
@@ -0,0 +1,4 @@
+#  36. lesson - Calling an API
+# Web service - place there a function
+# sharing the functionality, not the actual code
+# programmer with the function's address and permission can call it
