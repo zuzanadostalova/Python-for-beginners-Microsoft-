dee9c44a346d409999e99c1da89f1342630c9dd0
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2019-12-12 00:45:35 +0100

    ....

--- 40_41_Managing_keys.py
+++ 40_41_Managing_keys.py
@@ -1,19 +1,29 @@
 # 40. and 41. lesson - Managing keys
 # For Windows
-import os
-os_version = os.getenv("OS")
-print(os_version)
+# import os
+# os_version = os.getenv("OS")
+# print(os_version)
 
 # Using dotenv (.env)
 # to manage external things to our app, db connection strings, keys, passwords
 # store environmental variables in text file
 # do not check sensitive values into source control
 # .env file
-DATABASE=Sample_Connection_String
+# DATABASE=Sample_Connection_String
 
 # app.py
-from dotenv import load_dotenv
+# from environ import load_environ
+# load_environ()
 import os
-load_dotenv
-database = os.getenv(DATABASE)
-print(database)
+os.environ
+# password = os.environ("PASSWORD")
+# database = os.getenv(DATABASE)
+
+# print(password)
+
+# 1) create virtual environment
+# 2) requirement.txt file with python-dotenv inside
+# 3) pip3 install -r requirement.txt in the terminal in virtual environment set up previously
+# 4) .env key value pairs we are going to use = password
+# 5) .gitignore ... .env
+ 
