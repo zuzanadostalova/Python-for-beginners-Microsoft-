d0c787b4a6d16fd20e6d81b52fdf1ca58ed2701c
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2019-12-11 14:43:58 +0100

    ...

--- 38_39_JSON.py
+++ 38_39_JSON.py
@@ -7,6 +7,18 @@
 
 # how to write the code which will read  it?
 # JSON contains key pairs - either a key - subkeys, subvalues or key - tags, list of values
-# different methods to read through JSON whether it is key - value,
-# key - subkeys, key - oldest of values
+# different methods to read through JSON whether it is 1) key - value,
+# 2) key - subkeys, 3) key - a list of values
 
+# 1) key - value; request the key name 
+# import JSON library
+# print(results["requestId"])
+# output:"3e06b5ccceec4cd3b5e1112c5b2e5c60"
+
+# 2) key - subkeys; key and subkey name
+# print (results["color"]["dominantColorBackground"])
+# output:White
+
+# 3) key - a list of values
+# print(results["description"]["tags"][0])
+# output:bear
