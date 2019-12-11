130f5f5f7632a3c387c8c06d5783145f6df74a00
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2019-12-11 14:14:10 +0100

    ...

---
+++ 38_39_JSON.py
@@ -0,0 +1,12 @@
+# 38. and 39. lesson - JavaScript Object Notation
+# the output of calling API is in JSON
+# standard data format 
+# picture analyzed through analyze image service
+# JSON linters - to read JSON output with code, formatting making it easier
+# to read the output
+
+# how to write the code which will read  it?
+# JSON contains key pairs - either a key - subkeys, subvalues or key - tags, list of values
+# different methods to read through JSON whether it is key - value,
+# key - subkeys, key - oldest of values
+
